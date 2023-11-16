5722f5c6cba4a226d88cefd775b78499274c216a
jeocarlolubao <jeocarlolubao@gmail.com>
2023-11-16 15:11:20 +0800

    Update about section

--- src/lib/components/about/about.svelte
+++ src/lib/components/about/about.svelte
@@ -1,53 +1,48 @@
 <!-- About-->
 <section class="resume-section" id="about">
-	<div class="resume-section-content">
-		<h1 class="mb-0">
-			Jeo Carlo
-			<span class="text-primary">Lubao</span>
-		</h1>
-		<div class="subheading mb-5">
-			Davao City, Philippines |
-			<a href="mailto:jeooocarlo@gmail.com">jeooocarlo@gmail.com</a>
-		</div>
-		<p class="lead mb-5">
-			I am an IT professional with a primary focus on Android mobile
-			development, complemented by a deep passion for web and backend
-			development, along with UI/UX design. Beyond academia, I explore
-			diverse areas such as Algorithmic Music, Machine Learning, Game
-			Development, and IT Project Management.
+  <div class="resume-section-content">
+    <h1 class="mb-0">
+      Jeo Carlo
+      <span class="text-primary">Lubao</span>
+    </h1>
+    <div class="subheading mb-5">
+      Davao City, Philippines |
+      <a href="mailto:jeooocarlo@gmail.com">jeooocarlo@gmail.com</a>
+    </div>
+    <p class="lead mb-5">
+      I am an IT professional with a strong technical foundation fullstack Web
+      Development(React, Svelte), with experience in databases such as SQL,
+      Firebase, Supabase, and Pocketbase. Versatile in mobile development,
+      including Flutter, React Native, and native Android with Java and Kotlin.
 
-			<br />
-			<br />
-			My insatiable curiosity fuels my drive to constantly expand my knowledge
-			and embrace new concepts, empowering me to contribute to groundbreaking
-			projects that push the boundaries of technology and deliver innovative
-			solutions.
-		</p>
-		<div class="social-icons">
-			<a
-				class="social-icon"
-				target="_blank"
-				href="https://www.linkedin.com/in/jeo-carlo-lubao-a12328254/"
-				><i class="fab fa-linkedin-in" /></a
-			>
-			<a
-				class="social-icon"
-				target="_blank"
-				href="https://github.com/jeoooo"><i class="fab fa-github" /></a
-			>
-			<!-- <a class="social-icon" href="https://stackoverflow.com/users/11835600/jeo-carlo-lubao"><i
+      <br />
+      <br />
+      Eager to apply this diverse skill set in an entry-level role, demonstrating
+      a strong commitment to continuous learning and professional growth in the dynamic
+      field of programming.
+    </p>
+    <div class="social-icons">
+      <a
+        class="social-icon"
+        target="_blank"
+        href="https://www.linkedin.com/in/jeo-carlo-lubao-a12328254/"
+        ><i class="fab fa-linkedin-in" /></a
+      >
+      <a class="social-icon" target="_blank" href="https://github.com/jeoooo"
+        ><i class="fab fa-github" /></a
+      >
+      <!-- <a class="social-icon" href="https://stackoverflow.com/users/11835600/jeo-carlo-lubao"><i
                             class="fab fa-stack-overflow"></i></a> -->
-			<!-- <a class="social-icon" href="https://www.reddit.com/user/jeooooo"><i class="fab fa-reddit"></i></a> -->
-			<a
-				class="social-icon"
-				target="_blank"
-				href="https://www.behance.net/jeolubao"
-				><i class="fab fa-behance" /></a
-			>
-			<!-- <a class="social-icon" href="#!"><i class="fab fa-twitter"></i></a>
+      <!-- <a class="social-icon" href="https://www.reddit.com/user/jeooooo"><i class="fab fa-reddit"></i></a> -->
+      <a
+        class="social-icon"
+        target="_blank"
+        href="https://www.behance.net/jeolubao"><i class="fab fa-behance" /></a
+      >
+      <!-- <a class="social-icon" href="#!"><i class="fab fa-twitter"></i></a>
                     <a class="social-icon" href="#!"><i class="fab fa-facebook-f"></i></a> -->
-		</div>
-	</div>
+    </div>
+  </div>
 </section>
 
 <hr class="m-0" />
