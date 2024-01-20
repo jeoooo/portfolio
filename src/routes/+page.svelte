2d1ca7959fedb3e94ddb7e32fd21c3fd9213b08c
JEO_CARLO LUBAO <jeocarlolubao@gmail.com>
2024-01-20 22:29:19 +0800

    Update background color and particle settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,62 @@
 <script>
-  import Contact from "../lib/components/Contact.svelte";
-  import Accolades from "../lib/components/Accolades.svelte";
-  import Projects from "../lib/components/Projects.svelte";
-  import Education from "../lib/components/Education.svelte";
-  import Experience from "../lib/components/Experience.svelte";
-  import TechStack from "../lib/components/TechStack.svelte";
-  import Description from "../lib/components/Description.svelte";
-  import Introduction from "../lib/components/Introduction.svelte";
-  import Header from "$lib/components/Header.svelte";
-  import Footer from "$lib/components/Footer.svelte";
 </script>
 
-<Header />
-<Introduction />
-<Description />
-<TechStack />
-<Experience />
-<Education />
-<Projects />
-<Accolades />
-<Contact />
+<div class="h-10 w-fill m-3">
+  <div class="h-full w-fill lg:mx-[290px] flex flex-row">
+    <div class="flex flex-row h-fill gap-2">
+      <div class="h-full w-[100px] p-2 bg-slate-500">
+        <h1 class="">@jeooo</h1>
+      </div>
+      <div class="h-full w-[100px] p-2 bg-slate-500">
+        <h1 class="">blog</h1>
+      </div>
+      <div class="h-full w-[100px] p-2 bg-slate-500">
+        <h1 class="">contact</h1>
+      </div>
+    </div>
+  </div>
+</div>
 
-<Footer />
+<div
+  class="h-[250px] w-fill mx-[24px] md:md-[100px] lg:mx-[300px] bg-zinc-800 m-3 flex justify-center items-center"
+>
+  <div class="flex flex-col items-center">
+    <p class="text-6xl">Hello I'm Jeo! 👋</p>
+    <p class="text-xl mt-8">Software Developer | Graphic Designer</p>
+  </div>
+</div>
+
+<div
+  class="flex flex-row h-fill w-fill mx-[24px] md:md-[100px] lg:mx-[300px] bg-zinc-800 m-3 justify-center items-center"
+>
+  <ol class="m-[20px] flex flex-col w-full border-s">
+    <li class="mb-4 ms-4">
+      <div
+        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 start-[38.5px] sm:start-[315px] md:start-[38.5px] lg:start-[315px] border border-white"
+      ></div>
+      <h3 class="mb-2 text-3xl font-semibold text-gray-900 dark:text-white">
+        Position
+      </h3>
+      <h3
+        class=" text-md font-normal leading-none text-gray-400 dark:text-gray-500"
+      >
+        March 2022 - March 2022
+      </h3>
+
+      <p
+        class="text-base font-normal text-justify text-gray-500 dark:text-gray-400 mt-4"
+      >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae error
+        illo possimus exercitationem eos pariatur, a labore nostrum tenetur
+        corporis voluptatibus nihil commodi eveniet fugit odit soluta nulla
+        ducimus deserunt. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Quisquam non nam doloribus libero! Aspernatur, quis dolorem
+        suscipit eaque ut error soluta pariatur tempore, unde, iusto
+        reprehenderit ipsa vel sit! Libero? Omnis eos, repellendus ratione rerum
+        tenetur labore soluta doloremque eveniet, nemo assumenda aliquam odio
+        velit deleniti provident, sapiente cum earum expedita praesentium eum
+        hic vero laudantium nisi? Autem, eligendi numquam!
+      </p>
+    </li>
+  </ol>
+</div>
