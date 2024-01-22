7c20cf300e8d68d13f674ef467369938a4be2e85
JEO_CARLO LUBAO <jeocarlolubao@gmail.com>
2024-01-22 09:52:34 +0800

    Refactor layout and page components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,6 @@
   import data from "$lib/jeocarlolubao.json";
 </script>
 
-<header>
-  <div class="h-10 w-fill m-3">
-    <div class="h-full w-fill lg:mx-[290px] flex flex-row">
-      <div class="flex flex-row h-fill gap-2">
-        <div class="h-full w-[100px] p-2 bg-zinc-800">
-          <h1 class="text-white">@jeooo</h1>
-        </div>
-        <div class="h-full w-[100px] p-2 bg-zinc-800">
-          <h1 class="text-white">blog</h1>
-        </div>
-        <div class="h-full w-[100px] p-2 bg-zinc-800">
-          <h1 class="text-white">contact</h1>
-        </div>
-      </div>
-    </div>
-  </div>
-</header>
-
 <section class="flex flex-col gap-y-2 mb-20">
   <div
     class="h-full w-fill pt-10 pb-10 mx-[24px] md:md-[100px] lg:mx-[300px] bg-zinc-800 m-3 flex justify-center items-center"
@@ -356,35 +338,3 @@
     </ol>
   </div>
 </section>
-
-<footer>
-  <div class=" h-[80px] w-fill bg-zinc-800 justify-center items-center">
-    <div
-      class="flex w-fill h-full mx-[24px] md:md-[100px] lg:mx-[300px] items-center justify-center"
-    >
-      <p class="text-white inline-flex">
-        year. Made with
-        <img
-          class="ml-1 mt-1 h-5 mr-1"
-          alt="simple-icon"
-          src="https://cdn.simpleicons.org/svelte/fff"
-        />
-        +
-        <img
-          class="ml-1 mt-1 h-5 mr-1"
-          alt="simple-icon"
-          src="https://cdn.simpleicons.org/typescript/fff"
-        />
-        +
-        <a
-          class="ml-1 hover:underline"
-          href="https://vincentgarreau.com/particles.js/#default"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          particles.js
-        </a>
-      </p>
-    </div>
-  </div>
-</footer>
