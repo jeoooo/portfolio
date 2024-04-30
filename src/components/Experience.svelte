e91a98947823f8059e76c954d23e0351f614568a
jeoooo <jeocarlolubao@gmail.com>
2024-04-30 19:01:51 +0800

    Update font styles in Experience.svelte and jeocarlolubao.json

--- src/components/Experience.svelte
+++ src/components/Experience.svelte
@@ -5,32 +5,34 @@
 <div
   class=" h-fill w-fill mx-[24px] md:md-[100px] lg:mx-[300px] bg-zinc-800 m-3 justify-center items-center pb-[4px]"
 >
-  <h1 class="px-[24px] pt-8 pb-4 text-4xl text-white font-syne font-bold">
+  <h1 class="px-[24px] pt-8 pb-4 text-4xl text-white syne-600 font-bold">
     <i class="fa-solid fa-user mr-3" /> Experience
   </h1>
-  <ol class="m-[20px] flex flex-col w-fill border-s">
+  <ol class="my-[32px] mx-[64px] flex flex-col w-fill border-s">
     {#each data.experience as exp}
       <li class="mb-4 ms-4">
         <div
-          class="absolute w-3 h-3 bg-gray-200 rounded-full mt-3.5 start-[38.5px] sm:start-[315px] md:start-[38.5px] lg:start-[315px] border border-white"
+          class="absolute w-3 h-3 bg-gray-200 rounded-full mt-3.5 start-[38.5px] sm:start-[358px] md:start-[38.5px] lg:start-[358px] border border-white"
         ></div>
-        <h3 class="mb-1 text-3xl text-white font-syne font-bold">
+        <h3 class="mb-1 text-3xl text-white ibm-plex-sans-bold">
           {exp.title}
         </h3>
         {#if exp.company}
-          <h3 class="my-2 text-lg font-semibold text-white font-ibm-plex-sans">
+          <h3
+            class="my-2 text-lg font-semibold text-white ibm-plex-sans-semibold"
+          >
             {exp.company}
           </h3>
         {/if}
 
         <h3
-          class="my-3 text-sm font-normal leading-none text-gray-400 font-ibm-plex-sans"
+          class="my-3 text-sm font-normal leading-none text-gray-400 ibm-plex-sans-light"
         >
           {exp.duration}
         </h3>
         {#if exp.responsibilities}
           {#each exp.responsibilities.roleDescription as role (role)}
-            <p class="font-ibm-plex-sans text-justify text-gray-50 my-2">
+            <p class="ibm-plex-sans-regular text-justify text-gray-50 my-2">
               {role}
             </p>
           {/each}
