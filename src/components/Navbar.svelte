f0b177ad4166d2f6b178e16ca8fb15a515ee4e52
jeoooo <jeocarlolubao@gmail.com>
2024-05-01 08:47:47 +0800

    Update font styles in Navbar.svelte

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -23,36 +23,15 @@
         <li>
           <a
             href="#"
-            class="block py-2 px-3 text-white bg-zinc-800 rounded md:bg-transparent md:text-white md:p-0 dark:text-white md:dark:text-white"
+            class="block py-2 px-3 text-white bg-zinc-800 rounded ibm-plex-sans-semibold md:bg-transparent md:text-white md:p-0 dark:text-white md:dark:text-white"
             aria-current="page">Home</a
           >
         </li>
         <li>
           <a
             href="#"
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >About</a
-          >
-        </li>
-        <li>
-          <a
-            href="#"
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >Services</a
-          >
-        </li>
-        <li>
-          <a
-            href="#"
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >Pricing</a
-          >
-        </li>
-        <li>
-          <a
-            href="#"
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >Contact</a
+            class="block py-2 px-3 text-gray-900 rounded ibm-plex-sans-semibold hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >Blog</a
           >
         </li>
       </ul>
