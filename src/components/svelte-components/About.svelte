bd051d980009f9dfb66941e13d3ce63f26875b2f
jeoooo <jeocarlolubao@gmail.com>
2024-05-13 20:54:12 +0800

    refactor: Update About page layout and content

--- src/components/svelte-components/About.svelte
+++ src/components/svelte-components/About.svelte
@@ -1,58 +1,22 @@
-<script>
+<script lang="ts">
   import data from "../../jeocarlolubao.json";
 </script>
 
 <div
-  class=" h-fill w-fill mx-[24px] md:md-[100px] lg:mx-[300px] bg-zinc-800 m-3 justify-center items-center pb-[4px] mb-4"
+  class=" h-full w-fill mx-[24px] md:md-[100px] lg:mx-[300px] bg-zinc-800 m-3 justify-center items-center"
 >
-  <h1 class="px-[24px] pt-8 pb-4 text-4xl text-white font-bold syne-600">
-    <i class="fa-solid fa-award mr-3" /> Awards, Conferences, & Certifications
-  </h1>
-  <ol class="my-[32px] mx-[64px] flex flex-col w-fill border-s">
-    <li class="mb-4 ms-4">
-      <div
-        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-3.5 start-[38.5px] sm:start-[358px] md:start-[38.5px] lg:start-[358px] border border-white"
-      ></div>
-      <h3 class="mb-5 text-3xl font-semibold text-white syne-600">
-        Awards & Competitions
-      </h3>
-      {#each data.awardsConferencesCertifications.awardsCompetitions as award}
-        <h3 class="mb-2 text-white ibm-plex-sans-regular">
-          <i class="fa-solid fa-trophy mr-1" />
-          {award}
-        </h3>
-      {/each}
-    </li>
-    <li class="mb-4 ms-4">
-      <div
-        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-3.5 start-[38.5px] sm:start-[358px] md:start-[38.5px] lg:start-[358px] border border-white"
-      ></div>
-
-      <h3 class="mb-5 text-3xl font-semibold syne-600 text-white">
-        Seminars Attended
-      </h3>
-      {#each data.awardsConferencesCertifications.seminarsAttended as seminar}
-        <h3 class="mb-2 text-white ibm-plex-sans-regular">
-          <i class="fa-solid fa-certificate mr-1" />
-          {seminar}
-        </h3>
-      {/each}
-    </li>
-    <li class="mb-4 ms-4">
-      <div
-        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-3.5 start-[38.5px] sm:start-[358px] md:start-[38.5px] lg:start-[358px] border border-white"
-      ></div>
-      <h3 class="mb-5 text-3xl font-semibold syne-600 text-white">
-        Conferences
-      </h3>
-      {#each data.awardsConferencesCertifications.conferences as conference}
-        <h3 class="mb-2 text-white ibm-plex-sans-regular">
-          <i class="fa-solid fa-calendar-day mr-1" />
-          {conference}
-        </h3>
-      {/each}
-    </li>
-  </ol>
+  <div class="flex flex-col items-start">
+    <p class="px-[24px] pt-8 pb-4 text-4xl text-white syne-600 font-bold">
+      <i class="fa-solid fa-circle-question mr-3" />About Me
+    </p>
+  </div>
+  <div class="w-fit h-fit px-[20px] py-5 flex flex-row">
+    <p
+      class="text-zinc-50 text-lg ibm-plex-sans-regular px-5 pb-5 pt-0 text-justify"
+    >
+      {data.aboutMe}
+    </p>
+  </div>
 </div>
 
 <style>
