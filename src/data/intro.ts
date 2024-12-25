e1ba40de0bd0fdb4a3b9f5b4fc854957c4805c35
jeoooo <jeocarlolubao@gmail.com>
2024-12-25 19:12:57 +0800

    feat: initial intro section

--- src/data/intro.ts
+++ src/data/intro.ts
@@ -1,39 +1,43 @@
-export const profileData= {
+export const profileData = {
     name: "Jeo Carlo Lubao",
     description: "Software Developer | Graphic Designer",
     socials: [
-      {
-        iconName: "GitHub",
-        name: "GitHub",
-        link: "https://github.com/jeoooo",
-      },
-      {
-        iconName: "Linkedin",
-        name: "Linkedin",
-        link: "https://www.linkedin.com/in/jeoooo/",
-      },
-      {
-        iconName: "Behance",
-        name: "Behance",
-        link: "https://www.behance.net/jeolubao",
-      },
-      {
-        iconName: "medium",
-        name: "I write articles on Medium",
-        link: "https://medium.com/@jeooocarlo",
-      },
-      {
-        iconName: "BuyMeACoffee",
-        name: "Buy Me A Coffee!",
-        link: "https://www.buymeacoffee.com/jeooo",
-      },
-      {
-        iconName: "githubsponsors",
-        name: "Sponsor me on GitHub!",
-        link: "https://github.com/sponsors/jeoooo",
-      },
+        {
+            name: "GitHub",
+            link: "https://github.com/jeoooo",
+            imgSrc: "https://img.icons8.com/?size=100&id=106564&format=png&color=000000",
+        },
+        {
+            name: "Linkedin",
+            link: "https://www.linkedin.com/in/jeoooo/",
+            imgSrc: "https://img.icons8.com/?size=100&id=85348&format=png&color=000000",
+        },
+        {
+            name: "Behance",
+            link: "https://www.behance.net/jeolubao",
+            imgSrc: "https://img.icons8.com/?size=100&id=10889&format=png&color=000000",
+        },
+        {
+            name: "I write articles on Medium",
+            link: "https://medium.com/@jeooocarlo",
+            imgSrc: "https://img.icons8.com/?size=100&id=sAeVzKTJTawA&format=png&color=000000",
+        },
+        {
+            name: "Buy Me A Coffee!",
+            link: "https://www.buymeacoffee.com/jeooo",
+            imgSrc: "https://img.icons8.com/?size=100&id=InhPERL3gwUB&format=png&color=000000",
+        },
+        {
+            name: "Sponsor me on GitHub!",
+            link: "https://github.com/sponsors/jeoooo",
+            imgSrc: "https://img.icons8.com/?size=100&id=106564&format=png&color=000000",
+        },
     ],
     aboutMe:
-      "I am an IT professional with a primary focus on Android mobile development, complemented by a deep passion for web and backend development, along with UI/UX design. Beyond academia, I explore diverse areas such as Algorithmic Music, Machine Learning, Game Development, and IT Project Management. \n\n My insatiable curiosity fuels my drive to constantly expand my knowledge and embrace new concepts, empowering me to contribute to groundbreaking projects that push the boundaries of technology and deliver innovative solutions.",
-  };
-  
+        `I am an IT professional with a primary focus on mobile development, 
+        complemented by a deep passion for full stack development, along with 
+        UI/UX design. Beyond academia, I explore diverse areas such as 
+        Algorithmic Music, AI, and Game Development. \n\n 
+        Driven by curiosity, I continually expand my knowledge 
+        to contribute to innovative, boundary-pushing projects.`,
+};
