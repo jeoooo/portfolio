7be4cea01ca236705357fedae03c9b53cf8a9927
jeoooo <jeocarlolubao@gmail.com>
2024-12-25 20:56:56 +0800

    feat: add tech stack and design stack data

---
+++ src/data/stack.ts
@@ -0,0 +1,209 @@
+export const techStack = {
+    techStack: [
+        {
+            name: "svelte",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/svelte",
+        },
+        {
+            name: "next.js",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/nextdotjs/fff",
+        },
+        {
+            name: "androidstudio",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/androidstudio",
+        },
+        {
+            name: "flutter",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/flutter",
+        },
+        {
+            name: "firebase",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/firebase",
+        },
+        {
+            name: "react",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/react",
+        },
+        {
+            name: "kotlin",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/kotlin",
+        },
+        {
+            name: "astro",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/astro",
+        },
+        {
+            name: "jetpackcompose",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/jetpackcompose",
+        },
+        {
+            name: "arduino",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/arduino",
+        },
+        {
+            name: "c++",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/cplusplus",
+        },
+        {
+            name: "csharp",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=55205&format=png&color=5C2D91", // C# color - #5C2D91
+        },
+        {
+            name: "css3",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/css",
+        },
+        {
+            name: "docker",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/docker",
+        },
+        {
+            name: "dotnet",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/dotnet",
+        },
+        {
+            name: "html5",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/html5",
+        },
+        {
+            name: "java",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=13679&format=png&color=000000",
+        },
+        {
+            name: "javascript",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=108784&format=png&color=000000",
+        },
+        {
+            name: "laravel",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/laravel",
+        },
+        {
+            name: "mysql",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/mysql",
+        },
+        {
+            name: "mui",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/mui",
+        },
+        {
+            name: "nodedotjs",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/nodedotjs",
+        },
+        {
+            name: "php",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/php",
+        },
+        {
+            name: "pocketbase",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/pocketbase/fff",
+        },
+        {
+            name: "postman",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/postman",
+        },
+        {
+            name: "python",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=13441&format=png&color=000000",
+        },
+        {
+            name: "sqlite",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/sqlite",
+        },
+        {
+            name: "tailwindcss",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/tailwindcss",
+        },
+        {
+            name: "typescript",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/typescript",
+        },
+        {
+            name: "microsoftsqlserver",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=laYYF3dV0Iew&format=png&color=ffffff",
+        },
+        {
+            name: "git",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/git",
+        },
+        {
+            name: "github",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/github/fff",
+        },
+        {
+            name: "stripe",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/stripe",
+        },
+        {
+            name: "storybook",
+            color: "#fff",
+            imgSrc: "https://cdn.simpleicons.org/storybook",
+        }
+    ],
+};
+
+export const designStack = {
+    designStack: [
+        {
+            name: "adobephotoshop",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=13677&format=png&color=000000"
+        },
+        {
+            name: "adobeillustrator",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=13631&format=png&color=000000",
+        },
+        {
+            name: "adobexd",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=4VVL78edhbW9&format=png&color=000000",
+        },
+        {
+            name: "figma",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=zfHRZ6i1Wg0U&format=png&color=000000",
+        },
+        {
+            name: "canva",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=iWw83PVcBpLw&format=png&color=000000",
+        },
+        {
+            name: "adobeindesign",
+            color: "#fff",
+            imgSrc: "https://img.icons8.com/?size=100&id=13675&format=png&color=000000",
+        }
+    ],
+};
