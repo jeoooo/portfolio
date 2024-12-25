3a6385cc1ce240eea11407abeed8acb5cac15dc3
jeoooo <jeocarlolubao@gmail.com>
2024-12-25 18:22:01 +0800

    feat: add profile data with personal information and social links

---
+++ src/data/intro.ts
@@ -0,0 +1,39 @@
+export const profileData= {
+    name: "Jeo Carlo Lubao",
+    description: "Software Developer | Graphic Designer",
+    socials: [
+      {
+        iconName: "GitHub",
+        name: "GitHub",
+        link: "https://github.com/jeoooo",
+      },
+      {
+        iconName: "Linkedin",
+        name: "Linkedin",
+        link: "https://www.linkedin.com/in/jeoooo/",
+      },
+      {
+        iconName: "Behance",
+        name: "Behance",
+        link: "https://www.behance.net/jeolubao",
+      },
+      {
+        iconName: "medium",
+        name: "I write articles on Medium",
+        link: "https://medium.com/@jeooocarlo",
+      },
+      {
+        iconName: "BuyMeACoffee",
+        name: "Buy Me A Coffee!",
+        link: "https://www.buymeacoffee.com/jeooo",
+      },
+      {
+        iconName: "githubsponsors",
+        name: "Sponsor me on GitHub!",
+        link: "https://github.com/sponsors/jeoooo",
+      },
+    ],
+    aboutMe:
+      "I am an IT professional with a primary focus on Android mobile development, complemented by a deep passion for web and backend development, along with UI/UX design. Beyond academia, I explore diverse areas such as Algorithmic Music, Machine Learning, Game Development, and IT Project Management. \n\n My insatiable curiosity fuels my drive to constantly expand my knowledge and embrace new concepts, empowering me to contribute to groundbreaking projects that push the boundaries of technology and deliver innovative solutions.",
+  };
+  
