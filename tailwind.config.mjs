7e107404f90f315c6ed5551332e6228f8eae8322
jeoooo <jeocarlolubao@gmail.com>
2024-12-26 09:48:21 +0800

    feat: add global CSS file and configure Tailwind for custom Helvetica font families

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				helvetica: ['Helvetica', 'sans-serif'],
+				'helvetica-bold': ['Helvetica Bold', 'sans-serif'],
+				'helvetica-light': ['Helvetica Light', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
